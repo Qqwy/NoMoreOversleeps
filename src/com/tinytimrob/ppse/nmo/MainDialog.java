61a0ffcc3ebde8b8d03ee10cb89eb4767cac8bc4
Robert Dennington <tinytimrob@ginever.net>
2017-04-11 16:10:25 +0100

    Start work on the gui

--- src/com/tinytimrob/ppse/nmo/MainDialog.java
+++ src/com/tinytimrob/ppse/nmo/MainDialog.java
@@ -5,11 +5,19 @@ import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import com.tinytimrob.ppse.nmo.utils.JavaFxHelper;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Control;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.web.WebEngine;
@@ -30,7 +38,7 @@ public class MainDialog extends Application
 		Configuration.load();
 		try
 		{
-			Pavlok.vibration(255, "Connection test");
+			//Pavlok.vibration(255, "Connection test");
 		}
 		catch (Throwable t)
 		{
@@ -51,6 +59,7 @@ public class MainDialog extends Application
 		// CONFIGURE THE SCENE
 		//==================================================================
 		final StackPane outerPane = new StackPane();
+		outerPane.setId("root");
 		scene = new Scene(outerPane, 800, 600, Color.WHITE);
 		scene.getStylesheets().add(JavaFxHelper.buildResourcePath("application.css"));
 
@@ -58,6 +67,87 @@ public class MainDialog extends Application
 		// BUILD THE PRIMARY PANE
 		//==================================================================
 		final BorderPane innerPane = new BorderPane();
+		{ // Main container pane
+			innerPane.setStyle("-fx-background-color: #222;");
+		}
+
+		final HBox innerTopPane = JavaFxHelper.createHorizontalBox(Control.USE_COMPUTED_SIZE, 35, new Insets(8, 4, 8, 4));
+		{ // Top section
+			innerTopPane.setStyle("-fx-background-color: #414C7D;");
+			final Label label = JavaFxHelper.createLabel(" NoMoreOversleeps", Color.WHITE, "-fx-font-weight: bold; -fx-font-size:16;", new Insets(0, 0, 0, 3), 320, Control.USE_COMPUTED_SIZE);
+			label.setGraphic(JavaFxHelper.createIcon(FontAwesomeIcon.BED, "16", Color.WHITE));
+			innerTopPane.getChildren().add(label);
+			innerPane.setTop(innerTopPane);
+		}
+
+		final GridPane innerRightPane = new GridPane();
+		{ // Manual Pavlok controls
+			innerRightPane.setMinWidth(200);
+			innerRightPane.setMaxWidth(200);
+			innerRightPane.setStyle("-fx-background-color: #444;");
+			innerRightPane.setVgap(10);
+			innerRightPane.setPadding(new Insets(10, 10, 10, 10));
+			final Label label = JavaFxHelper.createLabel("Manual controls", Color.WHITE, "", new Insets(0, 0, 0, 3), 160, Control.USE_COMPUTED_SIZE);
+			final Button beepButton = JavaFxHelper.createButton("BEEP", JavaFxHelper.createIcon(FontAwesomeIcon.VOLUME_UP, "12", Color.BLACK));
+			beepButton.setMinWidth(180);
+			beepButton.setMaxWidth(180);
+			beepButton.setOnAction(new EventHandler<ActionEvent>()
+			{
+				@Override
+				public void handle(ActionEvent arg0)
+				{
+					try
+					{
+						Pavlok.beep(255, "Manually triggered beep");
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			});
+			final Button vibrateButton = new Button("VIBRATE");
+			vibrateButton.setMinWidth(180);
+			vibrateButton.setMaxWidth(180);
+			vibrateButton.setOnAction(new EventHandler<ActionEvent>()
+			{
+				@Override
+				public void handle(ActionEvent arg0)
+				{
+					try
+					{
+						Pavlok.vibration(255, "Manually triggered vibration");
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			});
+			final Button shockButton = new Button("SHOCK");
+			shockButton.setMinWidth(180);
+			shockButton.setMaxWidth(180);
+			shockButton.setOnAction(new EventHandler<ActionEvent>()
+			{
+				@Override
+				public void handle(ActionEvent arg0)
+				{
+					try
+					{
+						Pavlok.shock(255, "Manually triggered shock");
+					}
+					catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+				}
+			});
+			innerRightPane.addRow(0, label);
+			innerRightPane.addRow(1, beepButton);
+			innerRightPane.addRow(2, vibrateButton);
+			innerRightPane.addRow(3, shockButton);
+			innerPane.setRight(innerRightPane);
+		}
 
 		//==================================================================
 		// PAVLOK CRAP
