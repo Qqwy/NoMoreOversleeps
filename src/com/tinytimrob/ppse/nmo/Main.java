e103db363bb87f220133452674a616898bf397e3
Robert Dennington <tinytimrob@ginever.net>
2017-05-24 19:56:57 +0100

    Bump to 0.7

--- src/com/tinytimrob/ppse/nmo/Main.java
+++ src/com/tinytimrob/ppse/nmo/Main.java
@@ -32,7 +32,7 @@ import javafx.scene.text.Font;
 public class Main
 {
 	//-------------------------------------------
-	public static String VERSION = "0.6";
+	public static String VERSION = "0.7";
 	public static String JAVA_UPDATE_URL = "https://launcher.ginever.net/javaupdate";
 
 	//-------------------------------------------
