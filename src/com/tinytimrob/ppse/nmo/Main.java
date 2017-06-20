a60297cf15356aa1e8e5ecbbdcada462ba6a33a7
Robert Dennington <tinytimrob@ginever.net>
2017-06-20 08:10:51 +0100

    Bump to 0.9.2

--- src/com/tinytimrob/ppse/nmo/Main.java
+++ src/com/tinytimrob/ppse/nmo/Main.java
@@ -32,7 +32,7 @@ import javafx.scene.text.Font;
 public class Main
 {
 	//-------------------------------------------
-	public static String VERSION = "0.9.1";
+	public static String VERSION = "0.9.2";
 	public static String JAVA_UPDATE_URL = "https://launcher.ginever.net/javaupdate";
 
 	//-------------------------------------------
