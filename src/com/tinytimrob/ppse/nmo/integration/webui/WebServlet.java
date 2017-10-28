dae6c0d69655de6912018e5d8100c440e9af50f9
Qqwy/Wiebe-Marten Wijnja <qqwy@gmx.com>
2017-10-28 07:04:58 +0200

    WebUI buttons now handled over AJAX.

--- src/com/tinytimrob/ppse/nmo/integration/webui/WebServlet.java
+++ src/com/tinytimrob/ppse/nmo/integration/webui/WebServlet.java
@@ -185,7 +185,7 @@ public class WebServlet extends HttpServlet
 					Action action = actions.get(key);
 					if (!action.isSecret())
 					{
-						actionButtons += "<form method='POST' action='" + key + "'><button type='submit' class='btn btn-" + colours[colour] + "' style='width:286px;'>" + action.getName() + "</button></form>";
+						actionButtons += "<form method='POST' data-js-ajax-form='true' action='" + key + "'><button type='submit' class='btn btn-" + colours[colour] + "' style='width:286px;'>" + action.getName() + "</button></form>";
 					}
 				}
 			}
@@ -224,7 +224,13 @@ public class WebServlet extends HttpServlet
 				{
 					button.onAction();
 					MainDialog.triggerEvent("<" + button.getName() + "> from /" + request.getRemoteAddr(), null);
-					response.sendRedirect("/");
+					
+					// When calling through AJAX, no response HTML necessary
+					if (request.getParameter("ajax_form") != null) {
+						response.setStatus(HttpServletResponse.SC_OK);
+					} else {
+						response.sendRedirect("/");						
+					}
 					return;
 				}
 			}
